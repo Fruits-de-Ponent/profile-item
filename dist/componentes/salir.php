beb7857bce379a476d21e8d2cade1f0d256daa93
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-27 13:37:32 +0200

    Creando estilo del pop up salir

--- dist/componentes/salir.php
+++ dist/componentes/salir.php
@@ -2,13 +2,12 @@
     <div class="modal fade text-center" id="salir" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">¿Estás seguro de que quieres salir?</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="modal-header border-0 justify-content-center">
+                    <h5 class="modal-title text-color">¿Estás seguro de que quieres salir?</h5>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-color" data-bs-dismiss="modal">Cancelar</button>
-                    <a type="button" class="btn btn-danger" href="./logout.php">Salir</a>
+                <div class="modal-footer border-0 justify-content-center">
+                    <button type="button" class="btn btn-color-invertido" data-bs-dismiss="modal">Cancelar</button>
+                    <a type="button" class="btn btn-color" href="./logout.php">Salir</a>
                 </div>
             </div>
         </div>
