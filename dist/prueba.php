b1cea8e0ecafa2f67e3e6a7bf7e36adb6ecc01c7
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-27 12:04:05 +0200

    Añadiendo comentarios y funciones de iconos

--- dist/prueba.php
+++ dist/prueba.php
@@ -1,7 +1,7 @@
 <?php
+    include('./componentes/iconos.php');
     session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +13,7 @@
     <link href="./prueba.css" rel="stylesheet">
 </head>
 <body>
-
+    <!-- NAVBAR -->
     <nav class="navbar color text-light sticky-top border-bottom border-light shadow-sm" id="navegador">
         <div class="container-fluid">
             <div>
@@ -28,26 +28,54 @@
             </div>
         </div>
     </nav>
-
+    <!-- PANEL DE CONTROL -->
     <div class="container-fluid mt-3">
-        <h5 class="mx-1 text-color">PANEL DE CONTROL</h5>
+        <h5 class="mx-1 text-color"><b><?php generarIconPanel(); ?>PANEL DE CONTROL</b></h5>
         <div class="row">
             <div class="col d-grid gap-2 mx-1">
-                <button class="btn btn-color shadow" type="button" data-bs-toggle="collapse" data-bs-target="#colapsar-datos" aria-expanded="true" aria-controls="colapsar-datos">DATOS</button>
+                <button 
+                    class="btn btn-color shadow" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#colapsar-datos" 
+                    aria-expanded="true" 
+                    aria-controls="colapsar-datos">
+                    <?php generarIconDatos(); ?>
+                    DATOS
+                </button>
             </div>
             <div class="col d-grid gap-2 mx-1">
-                <button class="btn btn-color shadow" type="button" data-bs-toggle="collapse" data-bs-target="#colapsar-app" aria-expanded="false" aria-controls="colapsar-app">APP's</button>
+                <button 
+                    class="btn btn-color shadow" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#colapsar-app" 
+                    aria-expanded="false" 
+                    aria-controls="colapsar-app">
+                    <?php generarIconApp(); ?>
+                    APP's
+                </button>
             </div>
             <div class="col d-grid gap-2 mx-1">
-                <button class="btn btn-color shadow" type="button" data-bs-toggle="collapse" data-bs-target="#colapsar-config" aria-expanded="false" aria-controls="colapsar-config">CONFIGURACIÓN</button>
+                <button 
+                    class="btn btn-color shadow align-self-center" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#colapsar-config" 
+                    aria-expanded="false" 
+                    aria-controls="colapsar-config">
+                    <?php generarIconEngranaje();?>
+                    CONFIGURACIÓN
+                </button>
             </div>
         </div>
     </div>
 
+    <!-- DATOS -->
     <div class="collapse" id="colapsar-datos">
         <div class="container-fluid mt-3">
         <hr>
-            <h5 class="mx-1 text-color">DATOS</h5>
+            <h5 class="mx-1 text-color"><b><?php generarIconData(); ?>DATOS<b></h5>
             <div class="row mx-1 rounded color text-light py-2 pt-3 justify-content-center text-center shadow">
                 <div class="col border-end border-light">
                     <h5><b>Usuario</b></h5>
@@ -69,42 +97,38 @@
         </div>
     </div>
 
+    <!-- APP's -->
     <div class="collapse" id="colapsar-app">
         <div class="container-fluid mt-3">
             <hr>
-            <h5 class="mx-1 text-color">APP's</h5>
+            <h5 class="mx-1 text-color"><b><?php generarIconCaja(); ?>APP's</b></h5>
             <div class="row">
-                <div class="col d-grid gap-2">
+                <div class="col d-grid">
                     <button class="btn btn-color shadow">VACACIONES</button>
                 </div>
-                <div class="col d-grid gap-2">
+                <div class="col d-grid">
                     <button class="btn btn-color shadow">FORMACIONES</button>
                 </div>
-                <div class="col d-grid gap-2">
-                    <button class="btn btn-color shadow">ALTA TRABAJADOR</button>
+                <div class="col d-grid">
+                    <button class="btn btn-color shadow">GRAFICO RENDIMIENTO</button>
                 </div>
             </div>
             <div class="row mt-1">
-                <div class="col d-grid gap-2">
-                    <button class="btn btn-color shadow">CALENDARIO</button>
-                </div>
-                <div class="col d-grid gap-2">
-                    <button class="btn btn-color shadow">HORAS</button>
-                </div>
-                <div class="col d-grid gap-2">
-                    <button class="btn btn-color shadow">PAGINA PRINCIPAL</button>
+                <div class="col d-grid gap-1">
+                    <button class="btn btn-color shadow">TEST</button>
                 </div>
             </div>
         </div>
     </div>
 
+    <!-- CONFIGURACIÓN -->
     <div class="collapse" id="colapsar-config">
         <div class="container-fluid mt-3">
             <hr>
-            <h5 class="mx-1 text-color">CONFIGURACION</h5>
+            <h5 class="text-color"><b><?php generarIconLlave(); ?>CONFIGURACIÓN</b></h5>
             <div class="row">
+                <h6 class="text-color"><?php generarIconPass(); ?>Cambiar contraseña</h6>
                 <div class="col d-grid gap-2">
-                    <h6 class="mx-1 text-color">Cambiar contraseña</h6>
                     <div class="input-group">
                         <input type="text" class="form-control" placeholder="Introduce la nueva contraseña">
                     </div>
@@ -115,8 +139,8 @@
                 </div>
             </div>
             <div class="row mt-1">
+                <h6 class="text-color"><?php generarIconFoto(); ?>Cambiar imagen del perfil</h6>
                 <div class="col d-grid gap-2">
-                    <h6 class="mx-1 text-color">Cambiar imagen del perfil</h6>
                     <div class="input-group">
                         <input type="file" class="form-control">
                         <button class="btn btn-color" type="button">Cambiar</button>
@@ -130,18 +154,3 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
-
-
-<?php function generarIconLogOut() { ?>
-    <svg xmlns="http://www.w3.org/2000/svg" class="svg my-2 mx-1 salir"  width="30" alt="door" fill="currentColor" class="bi bi-door-closed" viewBox="0 0 16 16">
-        <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2zm1 13h8V2H4v13z"/>
-        <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0z"/>
-    </svg>
-<?php } ?>
-
-<?php function generarIconPerfil() { ?>
-    <svg xmlns="http://www.w3.org/2000/svg" class="svg my-2 mx-1" width="25" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-    </svg>
-<?php } ?>
