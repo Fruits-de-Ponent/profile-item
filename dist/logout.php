afca752a2105f34767d8d240780f33c5442515f5
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-27 13:08:39 +0200

    Eliminando decorador innecesario

--- dist/logout.php
+++ dist/logout.php
@@ -1,7 +1,7 @@
 <?php
-@session_start();
-session_destroy();
-header("Location: ../../vacaciones/index.html");
+    session_start();
+    session_destroy();
+    header("Location: ../../vacaciones/index.html");
 ?>
 
 
