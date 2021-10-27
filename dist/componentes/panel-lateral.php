bcd355276c7e5e2f7659a4102e8d69a5ecf4f352
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-27 13:37:19 +0200

    Funcion para generar el componente panel lateral

---
+++ dist/componentes/panel-lateral.php
@@ -0,0 +1,11 @@
+<?php function generarPanelLateral() { ?>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="panel-lateral">
+        <div class="offcanvas-header navbar color text-light">
+            <h5 class="offcanvas-title" id="offcanvasWithBackdropLabel">Panel de control</h5>
+            <button type="button" class="btn svg-fill" data-bs-dismiss="offcanvas" aria-label="Close"><?php generarIconCerrar();?></button>
+        </div>
+        <div class="offcanvas-body">
+            
+        </div>
+    </div>
+<?php } ?>
