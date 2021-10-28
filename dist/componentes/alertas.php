6f1953e28a0f691fe11c7fe5ee69a83fd0be5a64
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 11:32:58 +0200

    Estilando alerta

--- dist/componentes/alertas.php
+++ dist/componentes/alertas.php
@@ -2,8 +2,7 @@
     <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
         <div id="alert-password" class="toast align-items-center text-color border-color" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="d-flex">
-                    <div class="toast-body"></div>
-                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                <div id="alerta-texto" class="toast-body text-color"></div>
             </div>
         </div>
     </div>
