ee0f9b97ab76bdcb5fc45d1427a03daa11f422dd
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 13:05:47 +0200

    Generando campana

--- dist/componentes/alertas.php
+++ dist/componentes/alertas.php
@@ -2,7 +2,7 @@
     <div class="position-fixed top-0 start-50 translate-middle-x mt-1" style="z-index: 1021">
         <div id="alert-password" class="toast align-items-center text-color border-color" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="d-flex">
-                <div id="alerta-texto" class="toast-body text-color"></div>
+                <div id="alerta-texto" class="toast-body text-color"><?php generarIconNotificacion(); ?></div>
             </div>
         </div>
     </div>
