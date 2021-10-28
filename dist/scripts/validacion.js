72d8547e84a591f2349df1576a44ecdd50c907f2
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 13:59:32 +0200

    Añadiendo validacion de medida de imagen

--- dist/scripts/validacion.js
+++ dist/scripts/validacion.js
@@ -1,14 +1,17 @@
 let campana = $('#alerta-texto').html()
-
 let params = (new URL(document.location)).searchParams;
 params = params.get("pass");
+window.history.pushState({}, document.title, "/profile-item/dist/prueba.php");
 
 if (params === 'actualizada') {
     alerta('Contraseña cambiada correctamente');
 }
 
 $('#cambiarImagenSubmit').on('click', () => {
-    
+    if (validateFormat($('#archivoImg').val()) || validateSize($('#archivoImg'))) {
+        console.log('asd')
+        $('#cambiarImagenForm').submit();
+    }
 })
 
 $('#cambiarPassSubmit').on('click', (e) => {
@@ -30,4 +33,39 @@ function alerta(texto) {
     $('.toast').fadeIn('slow', () => {
         $('.toast').delay(1500).fadeOut('slow');
     })
+}
+
+function validateSize() {
+    let subir;
+    var _URL = window.URL || window.webkitURL;
+    var element = $("#archivoImg");
+        var file, img;
+        if ((file = element[0].files[0])) {
+            img = new Image();
+            img.onload = function() {
+                if(this.width > 300 || this.height > 300) {
+                    console.log(this.width, this.height)
+                    alerta('La imagen mide demasiado. Medida máxima: 150px * 150px')
+                    subir = false;
+                } else {
+                    subir = true;
+                }
+            };
+            img.src = _URL.createObjectURL(file);
+            console.log('asdasasdd', true)
+            return subir;
+    }
+}
+
+function validateFormat(file) {
+    var ext = file.split(".");
+    ext = ext[ext.length-1].toLowerCase();      
+    var arrayExtensions = ["jpg" , "jpeg", "png", "bmp"];
+
+    if (arrayExtensions.lastIndexOf(ext) == -1) {
+        return false;
+    } else {
+        console.log('asdasd')
+        return true;
+    }
 }
