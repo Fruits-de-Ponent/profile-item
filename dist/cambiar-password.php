e93723c6f9ae57c5a85a7ccdfb55abf76b851c0d
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 09:10:42 +0200

    Cambiando updatePass a cambiar-password.php

---
+++ dist/cambiar-password.php
@@ -0,0 +1,7 @@
+
+<?php
+    session_start();
+    $trabajador=$_SESSION['Trabajador'];
+    require 'database.php';
+    $qry = mysqli_query($con,"UPDATE `login` SET `Password`='".$_POST['password']."' WHERE DNI='$trabajador'");
+?>
