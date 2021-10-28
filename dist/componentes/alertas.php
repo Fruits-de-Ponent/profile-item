f8739c8af72be3adf655399d5378285ad0cfba59
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 10:12:09 +0200

    Cambios en la funcion de generarAlerta

--- dist/componentes/alertas.php
+++ dist/componentes/alertas.php
@@ -1,8 +1,8 @@
-<?php function generarAlerta($texto){ ?>
+<?php function generarAlerta(){ ?>
     <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
-        <div id="liveToast" class="toast align-items-center text-white color border-0" role="alert" aria-live="assertive" aria-atomic="true">
+        <div id="liveToast" class="toast align-items-center text-color border-color" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="d-flex">
-                    <div class="toast-body"><?php echo $texto; ?></div>
+                    <div class="toast-body"></div>
                 <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
         </div>
