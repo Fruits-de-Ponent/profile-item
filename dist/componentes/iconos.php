c30bac7cd828ef15732889b21f09ad6aa1985c47
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 12:58:39 +0200

    Añadiendo nuevo icono

--- dist/componentes/iconos.php
+++ dist/componentes/iconos.php
@@ -93,7 +93,14 @@
 <?php } ?>
 
 <?php function generarIconCerrar() { ?>
-    <svg xmlns="http://www.w3.org/2000/svg" class="m-1 text-light" width="20" fill="currentColor" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+    <svg xmlns="http://www.w3.org/2000/svg" class="m-1" width="20" fill="currentColor" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5zM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5z"/>
     </svg>
 <?php } ?> 
+
+<?php function generarIconCheck() { ?>
+    <svg xmlns="http://www.w3.org/2000/svg" class="m-1" width="15" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+        <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
+        <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
+    </svg>
+<?php } ?>
