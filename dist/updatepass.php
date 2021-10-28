fb317ddba400af0cbb7da3ca255e3934148d067f
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 09:10:56 +0200

    Eliminando archivos no utilizados

--- dist/updatepass.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-// Recogemos el dni del trabajador que lo tenemos previamente guardado desde el login.
-session_start();
-$trabajador=$_SESSION['Trabajador'];
-require 'database.php';
-// creamos la consulta y la contraseña la recogemos de la pagina anterior
-$qry = mysqli_query($con,"UPDATE `login` SET `Password`='".$_POST['password']."' WHERE DNI='$trabajador'");
-?>
