c181652379267006fe160e1903666d6943aaf7bb
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 12:45:32 +0200

    Cambiando saltos de linea y espacios

--- dist/prueba.php
+++ dist/prueba.php
@@ -175,9 +175,9 @@
     </div>
 </div>
 
-    <?php generarPopSalir(); ?>
-    <?php generarPanelLateral(); ?>
-    <?php generarAlerta(); ?>
+<?php generarPopSalir(); ?>
+<?php generarPanelLateral(); ?>
+<?php generarAlerta(); ?>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
