48697060d3dd273794f44b11ce36a3bed1aa7f5f
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 13:06:35 +0200

    Añadiendo mas funcionalidades con Javascript

--- dist/scripts/validacion.js
+++ dist/scripts/validacion.js
@@ -1,23 +1,33 @@
+let campana = $('#alerta-texto').html()
+
+let params = (new URL(document.location)).searchParams;
+params = params.get("pass");
+
+if (params === 'actualizada') {
+    alerta('Contraseña cambiada correctamente');
+}
+
 $('#cambiarImagenSubmit').on('click', () => {
     
 })
 
 $('#cambiarPassSubmit').on('click', (e) => {
     e.preventDefault();
-    if (($('#passwordOne').val() != '' && $('#passwordTwo').val() != '')  && (($('#passwordOne').val()).length > 0 && ($('#passwordTwo').val()).length > 0)) {
+    if (($('#passwordOne').val() != '' && $('#passwordTwo').val() != '')  && (($('#passwordOne').val()).length > 3 && ($('#passwordTwo').val()).length > 3)) {
         if ($('#passwordOne').val() == $('#passwordTwo').val()) {
             $('#cambiarPassForm').submit();
         } else{
             alerta('Las contraseñas no coinciden.');
         }
     } else {
-        alerta('Los campos no pueden estar vacios y la contraseña deben tener mínimo 4 caracteres');
+        alerta('Los campos no pueden estar vacíos y la contraseña deben tener mínimo 4 caracteres.');
     }
 })
 
+
 function alerta(texto) {    
-    $('#alerta-texto').html(texto);
+    $('#alerta-texto').html(campana + texto);
     $('.toast').fadeIn('slow', () => {
         $('.toast').delay(1500).fadeOut('slow');
-    });
-};
+    })
+}
