925d60e443dc39c74d7e249ebdba9697d1d74075
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 12:44:49 +0200

    Cambios en la posicion de la alerta

--- dist/componentes/alertas.php
+++ dist/componentes/alertas.php
@@ -1,5 +1,5 @@
 <?php function generarAlerta(){ ?>
-    <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+    <div class="position-fixed top-0 start-50 translate-middle-x mt-1" style="z-index: 1021">
         <div id="alert-password" class="toast align-items-center text-color border-color" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="d-flex">
                 <div id="alerta-texto" class="toast-body text-color"></div>
