2bf61938ae871890cf53a0cd01f60c781b25b443
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 12:58:06 +0200

    Moviendo etiqueta title

--- dist/prueba.php
+++ dist/prueba.php
@@ -11,11 +11,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>Perfil</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perfil</title>
-    <link rel="icon" href="./imagenes/logo-200.png">
+    <link rel="icon" href="./assets/img/logo-200.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="./assets/css/prueba.css" rel="stylesheet">
     <link href="./assets/css/scrollbar.css" rel="stylesheet">
