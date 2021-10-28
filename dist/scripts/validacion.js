def2ca097f3d1137c59a660dac0353f8f63dbed1
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 12:45:12 +0200

    Funcionamiento y validacion de cambiar contraseña

--- dist/scripts/validacion.js
+++ dist/scripts/validacion.js
@@ -2,21 +2,22 @@ $('#cambiarImagenSubmit').on('click', () => {
     
 })
 
-$('#cambiarPassSubmit').on('click', () => {
-    if ($('#password-one').val() != '' && $('#password-two').val() != '') {
-        if ($('#password-one').val() == $('#password-two').val()) {
-            
+$('#cambiarPassSubmit').on('click', (e) => {
+    e.preventDefault();
+    if (($('#passwordOne').val() != '' && $('#passwordTwo').val() != '')  && (($('#passwordOne').val()).length > 0 && ($('#passwordTwo').val()).length > 0)) {
+        if ($('#passwordOne').val() == $('#passwordTwo').val()) {
+            $('#cambiarPassForm').submit();
         } else{
-            alerta('Las contraseñas no coinciden');
+            alerta('Las contraseñas no coinciden.');
         }
     } else {
-        alerta('Debes rellenar ambos campos');
+        alerta('Los campos no pueden estar vacios y la contraseña deben tener mínimo 4 caracteres');
     }
 })
 
-function alerta(texto) {
-    $('.toast').fadeOut('slow', () => {
-        $('#alerta-texto').html(texto);
-        $('.toast').fadeIn('slow').delay(3000).fadeOut('slow');
-    })
-}
+function alerta(texto) {    
+    $('#alerta-texto').html(texto);
+    $('.toast').fadeIn('slow', () => {
+        $('.toast').delay(1500).fadeOut('slow');
+    });
+};
