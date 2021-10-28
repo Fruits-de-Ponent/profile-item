41cf6ea7fe0abe93c7340092fd74c0a8906d3064
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 11:32:37 +0200

    Añadiendo condiciones y validaciones de contraseña

--- dist/scripts/validacion.js
+++ dist/scripts/validacion.js
@@ -1,7 +1,22 @@
-$('#cambiarImgSubmit').on('click', () => {
-    console.log('Click cambiar img')
+$('#cambiarImagenSubmit').on('click', () => {
+    
 })
 
 $('#cambiarPassSubmit').on('click', () => {
-    console.log('Click cambiar pass')
+    if ($('#password-one').val() != '' && $('#password-two').val() != '') {
+        if ($('#password-one').val() == $('#password-two').val()) {
+            
+        } else{
+            alerta('Las contraseñas no coinciden');
+        }
+    } else {
+        alerta('Debes rellenar ambos campos');
+    }
 })
+
+function alerta(texto) {
+    $('.toast').fadeOut('slow', () => {
+        $('#alerta-texto').html(texto);
+        $('.toast').fadeIn('slow').delay(3000).fadeOut('slow');
+    })
+}
