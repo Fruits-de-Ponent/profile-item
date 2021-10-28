7afddf730dc7c2793875ec65bf4814258d8710a0
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-28 09:44:11 +0200

    Creando estilo y estructura de alerta

--- dist/componentes/alertas.php
+++ dist/componentes/alertas.php
@@ -1,8 +1,10 @@
 <?php function generarAlerta($texto){ ?>
-    <div class="toast align-items-center text-white color border-0" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="d-flex">
-                <div class="toast-body"><?php $texto; ?></div>
-            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+    <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+        <div id="liveToast" class="toast align-items-center text-white color border-0" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="d-flex">
+                    <div class="toast-body"><?php echo $texto; ?></div>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
         </div>
     </div>
 <?php } ?> 
