b9ae37c8c60511bb9278c03f45b315fbafaceafa
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-29 11:55:46 +0200

    Añadiendo nueva funcion con icono

--- dist/componentes/iconos.php
+++ dist/componentes/iconos.php
@@ -110,3 +110,9 @@
         <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
     </svg>
 <?php } ?>
+
+<?php function generarIconPanelLateral() { ?>
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+    </svg>
+<?php } ?>
