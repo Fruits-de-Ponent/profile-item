87ff5149ba65506e8bc57251ff7e574b72f7248a
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-29 11:47:58 +0200

    Añadiendo script

--- dist/index.php
+++ dist/index.php
@@ -183,5 +183,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="./scripts/validacion.js"></script>
+<script src="./scripts/parametros.js"></script>
 </body>
 </html>
